687e57763db6dcd69cc5921157a8b908fc4dd76b
Joel <joeldario9912@gmail.com>
2024-01-03 19:05:23 -0300

    varios zod schema

--- src/schemas/productos/varios.ts
+++ src/schemas/productos/varios.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
 import { ProductoSchema } from "../producto";
 
-const VariosCrearSchema = ProductoSchema.extend({});
+const VariosCrearSchema = ProductoSchema.extend({
+  detalles: z.string(),
+});
 
 export const validarVariosCrear = (object: unknown) =>
   VariosCrearSchema.parse(object);

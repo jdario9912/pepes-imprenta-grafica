acb20deafb401309496c0240caeb779096c53305
Joel <joeldario9912@gmail.com>
2024-01-03 18:35:49 -0300

    remeras zod schema

--- src/schemas/productos/remeras.ts
+++ src/schemas/productos/remeras.ts
@@ -1,7 +1,14 @@
 import { z } from "zod";
 import { ProductoSchema } from "../producto";
 
-const RemerasCrearSchema = ProductoSchema.extend({});
+const RemerasCrearSchema = ProductoSchema.extend({
+  talles: z.string(),
+  color: z.string(),
+  estampa_pecho: z.string(),
+  estampa_espalda: z.string(),
+  color_estampa: z.string(),
+  cantidad: z.number().positive(),
+});
 
 export const validarRemerasCrear = (object: unknown) =>
   RemerasCrearSchema.parse(object);

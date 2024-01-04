8a0fdeaa6c1738de46510a2daacec4fd3f2e3d9c
Joel <joeldario9912@gmail.com>
2024-01-04 15:45:44 -0300

    querys http

--- src/app/api/ordenes/bonos/route.ts
+++ src/app/api/ordenes/bonos/route.ts
@@ -1,9 +1,14 @@
+import { errorResponse } from "@/libs/api/responses";
 import { NextResponse, NextRequest } from "next/server";
 
 export const POST = async (req: NextRequest) => {
-  const body = await req.json()
-  
-  console.log(body);
-  
-  return NextResponse.json("creando un producto")
-}
+  try {
+    const body = await req.json();
+
+    console.log(body);
+  } catch (error) {
+    return errorResponse(error)
+  }
+
+  return NextResponse.json("creando un producto");
+};
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,35 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
  
 export function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname.startsWith('/api/ordenes')) {
-    console.log("esta peticion va para un tipo de orden");
-
-    // aca tengo que validar si el path tiene un tipo de orden real
+  try {
+    if (req.nextUrl.pathname.startsWith('/api/ordenes')) {
+      console.log("esta peticion va para un tipo de orden");
+  
+      // aca tengo que validar si el path tiene un tipo de orden real
+      
+      // en una peticion post validar que el producto existe
+      // producto: z.enum([
+      //   "bonos",
+      //   "disenos",
+      //   "impresiones",
+      //   "loma",
+      //   "plotter",
+      //   "remeras",
+      //   "sellos",
+      //   "talonarios",
+      //   "tarjetas",
+      //   "varios",
+      //   "volantes",
+      // ]),
+    }
     
-    // en una peticion post validar que el producto existe
-    // producto: z.enum([
-    //   "bonos",
-    //   "disenos",
-    //   "impresiones",
-    //   "loma",
-    //   "plotter",
-    //   "remeras",
-    //   "sellos",
-    //   "talonarios",
-    //   "tarjetas",
-    //   "varios",
-    //   "volantes",
-    // ]),
+    if (req.nextUrl.pathname.startsWith('/api/clientes')) {
+      console.log("esta peticion va para los clientes");
+      
+    }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json("algo salio mal")
   }
   
-  if (req.nextUrl.pathname.startsWith('/api/clientes')) {
-    console.log("esta peticion va para los clientes");
-    
-  }
 }

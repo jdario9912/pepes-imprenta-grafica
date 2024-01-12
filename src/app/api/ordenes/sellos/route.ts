9f7b91ebdc5d2be7a7007d1531f419f22ad3b164
Joel <joeldario9912@gmail.com>
2024-01-12 00:15:40 -0300

    sellos crear

--- src/app/api/ordenes/sellos/route.ts
+++ src/app/api/ordenes/sellos/route.ts
@@ -1,11 +1,20 @@
+import { errorResponse } from "@/libs/api/responses";
+import { RemerasModel } from "@/models/mysql/productos/remeras";
+import { SellosModel } from "@/models/mysql/productos/sellos";
+import { validarRemerasCrear } from "@/schemas/productos/remeras";
+import { validarSellosCrear } from "@/schemas/productos/sellos";
 import { NextResponse, NextRequest } from "next/server";
 
-export const POST = async (req: NextRequest, { params }: any) => {
-  const body = await req.json()
-  
-  console.log(params);
-  
-  console.log(body);
-  
-  return NextResponse.json("creando un producto")
-}
+export const POST = async (req: NextRequest) => {
+  try {
+    const body = await req.json();
+
+    const ordenValidada = validarSellosCrear(body);
+    
+    const ordenCreada = await SellosModel.crear(ordenValidada);
+
+    return NextResponse.json(ordenCreada);
+  } catch (error) {
+    return errorResponse(error);
+  }
+};

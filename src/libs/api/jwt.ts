d21ebb9487d63b8c37f93abff348a94137d40e33
Joel <joeldario9912@gmail.com>
2024-01-29 13:49:15 -0300

    verificar token

--- src/libs/api/jwt.ts
+++ src/libs/api/jwt.ts
@@ -1,30 +1,25 @@
 import jwt from "jsonwebtoken";
 import { jwtSecret } from "@/config";
+import { JWTPayload, jwtVerify } from "jose";
 
-export const generarToken = async (dataToken: EmpleadoDataToken): Promise<string> => {
+export const generarToken = async (
+  dataToken: EmpleadoDataToken
+): Promise<string> => {
   return new Promise<string>((resolve, reject) => {
-    jwt.sign(
-      dataToken,
-      jwtSecret,
-      { expiresIn: "12h" },
-      (error, token) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(token || "");
-        }
+    jwt.sign(dataToken, jwtSecret, { expiresIn: "12h" }, (error, token) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(token || "");
       }
-    );
+    });
   });
 };
 
-export const validarToken = async (token: string): Promise<boolean | Error> => {
-  return new Promise((resolve, reject) => {
-    try {
-      jwt.verify(token, jwtSecret);
-      resolve(true)
-    } catch (error) {
-      throw new Error("Token invalida.")
-    }
-  });
+export const validarToken = async (token: string) => {
+  try {
+    await jwtVerify(token, new TextEncoder().encode(jwtSecret));
+  } catch (error) {
+    throw new Error("Token invalida.");
+  }
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,21 @@
-import { NextResponse, type NextRequest } from 'next/server'
-import { validarToken } from './libs/api/jwt';
- 
+import { NextResponse, type NextRequest } from "next/server";
+import { validarToken } from "./libs/api/jwt";
+import { errorResponse } from "./libs/api/responses";
+
 export async function middleware(req: NextRequest) {
-  
-  // return NextResponse.json("esto nunca llegara al backend")  
-  // const tokenValido = await validarToken(token);
+  const pathAuthQuery = req.nextUrl.pathname.startsWith("/api/auth/login");
+
+  if (!pathAuthQuery) {
+    const headers = new Headers(req.headers);
+    const token = headers.get("Authorization");
+
+    if (!token) return NextResponse.json({ mensaje: "No estas autorizado." });
+
+    try {
+      await validarToken(token);
+      NextResponse.next();
+    } catch (error) {
+      return errorResponse(error);
+    }
+  }
 }

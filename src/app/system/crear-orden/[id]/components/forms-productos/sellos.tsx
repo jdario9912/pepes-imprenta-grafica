86289af5ab9fa833acb027cf02b3a62a47cb9759
Joel <joeldario9912@gmail.com>
2024-02-03 23:37:59 -0300

    inputs sellos

--- src/app/system/crear-orden/[id]/components/forms-productos/sellos.tsx
+++ src/app/system/crear-orden/[id]/components/forms-productos/sellos.tsx
@@ -1,6 +1,18 @@
+import { sellosTipo } from "@/libs/listas";
+import InputSelect from "../input-select";
+import TextArea from "../text-area";
+
 const SellosForm = () => {
-  return(
-    <></>
+  return (
+    <>
+      <InputSelect label="tipo" opciones={sellosTipo} />
+
+      <TextArea label="tamaño" />
+
+      <TextArea label="texto" />
+
+      <TextArea label="dibujo" />
+    </>
   );
 };
 

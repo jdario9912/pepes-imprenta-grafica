422b64f2efa0e01007abbd6c03019bed1533ac22
Joel <joeldario9912@gmail.com>
2024-02-03 23:34:29 -0300

    inputs remeras

--- src/app/system/crear-orden/[id]/components/forms-productos/remeras.tsx
+++ src/app/system/crear-orden/[id]/components/forms-productos/remeras.tsx
@@ -1,5 +1,24 @@
+import InputNum from "../input-num";
+import InputText from "../input-text";
+
 const RemerasForm = () => {
-  return(<></>);
+  return (
+    <>
+      <InputText label="ubicación del archivo" />
+
+      <InputText label="talles" />
+
+      <InputText label="color" />
+
+      <InputText label="estampa pecho" />
+
+      <InputText label="estampa espalda" />
+
+      <InputText label="color estampa" />
+
+      <InputNum label="cantidad" />
+    </>
+  );
 };
 
 export default RemerasForm;

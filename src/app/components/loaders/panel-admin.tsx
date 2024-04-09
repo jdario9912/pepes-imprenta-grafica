4720cf5376b609359a09b4b2221a095477f618c8
Joel <joeldario9912@gmail.com>
2024-04-08 22:05:39 -0300

    pagina not found

--- src/app/components/loaders/panel-admin.tsx
+++ src/app/components/loaders/panel-admin.tsx
@@ -2,7 +2,7 @@ import NombrePaginaLoader from "./nombre-pagina";
 import TextLoader from "./text";
 
 const PanelAdminLoader = () => (
-  <div className="h-screen">
+  <div className="h-screen p-4">
     <NombrePaginaLoader />
     <div className="border border-slate-200 rounded-lg mt-8 p-4 overflow-hidden w-full h-1/2">
       <div className="flex gap-x-4">
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,36 @@
+import { iconos } from "@/components/icons";
 import Session from "@/components/session";
 import { Button } from "@nextui-org/react";
 import Link from "next/link";
 
 const HomeNotFound = async () => (
-  <div>
-    <h2>not Found. 404!</h2>
-    <p>La pagina no existe o fue borrada!</p>
-    <Button as={Link} href="/">
-      Inicio
-    </Button>
-    <Session>
-      <Button as={Link} href="/system">
-        Sistema
-      </Button>
-    </Session>
+  <div className="flex flex-col justify-center items-center h-screen">
+    <div className="mx-auto w-full text-center">
+      <h2 className="bg-gradient-to-r from-red-100/50 via-red-200 to-red-100/50">
+        <span className="font-black text-danger bg-white p-2 text-4xl">404</span>
+      </h2>
+      <p className="font-extrabold text-slate-700 text-4xl mt-6">
+        Página no encontrada
+      </p>
+      <p className="text-slate-500 italic mt-2">Ésta página no existe o fue borrada.</p>
+
+      <div className="flex items-center justify-center gap-4 mt-8">
+        <Button
+          as={Link}
+          href="/"
+          startContent={iconos.home}
+          color="primary"
+          variant="flat"
+        >
+          inicio
+        </Button>
+        <Session>
+          <Button as={Link} href="/system" color="primary" variant="solid">
+            sistema
+          </Button>
+        </Session>
+      </div>
+    </div>
   </div>
 );
 
